c512e23ac81d0c3c6c785c58775344f9a9c6e371
Oliver Karger <kmaster@oliver-karger.de>
2021-07-19 16:24:42 +0200

    'Hello World' Exampled added
    Signed-off-by: Oliver Karger <kmaster@oliver-karger.de>

---
+++ hello-world/hello_world.cbl
@@ -0,0 +1,10 @@
+       identification division.
+           program-id. hello_world.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+           display "Hello, World!".
+
+       end program hello_world.
---
+++ hello-world/run.sh
@@ -0,0 +1,2 @@
+cobc -x -o hello_world.cbl-appl ./hello_world.cbl
+./hello_world.cbl-appl
